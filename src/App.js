bb75d52e49e930f61159a6f9d0f50c165f91f1c8
SUNDAY SAMUEL <orinamesunday360@gmail.com>
2022-12-14 18:03:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Product";
+import ContactUs from "./components/pages/ContactUs";
+import SignUp from "./components/pages/SignUp";
+import Marketing from "./components/pages/Marketing";
+import Consulting from "./components/pages/Consulting";
+import About from "./components/pages/About";
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/contact-us" component={ContactUs} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path="/marketing" component={Marketing} />
+        <Route path="/consulting" component={Consulting} />
+         <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  );
+}
+export default App;
